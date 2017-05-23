4f3890e62676ec59fcbf31db70a0e620b5bd4042
Oli Larkin <olilarkin@googlemail.com>
2017-05-24 00:06:07 +0100

    Initial Commit

---
+++ VoiceCodeXCSourceEditorExtension/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  VoiceCodeXCSourceEditorExtension
+//
+//  Created by Oliver Larkin on 24/05/2017.
+//  Copyright © 2017 OliLarkin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
