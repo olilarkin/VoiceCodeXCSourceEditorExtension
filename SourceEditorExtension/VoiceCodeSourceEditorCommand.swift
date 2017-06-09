ad82991d4e4792b93ff1cc0ec9aa8fc8ee59ed52
Oli Larkin <olilarkin@googlemail.com>
2017-06-09 21:00:22 +0100

    web socket  connection working
    now need to parse json string

--- SourceEditorExtension/VoiceCodeSourceEditorCommand.swift
+++ SourceEditorExtension/VoiceCodeSourceEditorCommand.swift
@@ -21,6 +21,18 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
   deinit {
   }
   
+//  func convertToDictionary(text: String) -> [String: Any]? {
+//    if let data = text.data(using: .utf8) {
+//      do {
+//        return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+//      } catch {
+//        print(error.localizedDescription)
+//      }
+//    }
+//    return nil
+//  }
+
+  
   func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void) -> Void
   {
     let handler: (Error) -> () = { error in
@@ -31,8 +43,74 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
     
     service.get_latest_command() { (command) in
       let buffer = invocation.buffer
-      NSLog(buffer.contentUTI)
+      //NSLog(buffer.contentUTI)
       NSLog(command)
+      
+//      let dict = convertToDictionary(text: command)
+//
+//      switch(dict!["id"]) {
+//
+//        case "initial-state":
+//          NSLog("initial state - not handled")
+//          break
+//        case "editor:move-to-line-number":
+//          let newRange = XCSourceTextRange()
+//          newRange.start.column = 0
+//          newRange.start.line = 3
+//          newRange.end.column = 0
+//          newRange.end.line = 3
+//
+//          buffer.selections[0] = newRange
+//          break
+//        case "editor:move-to-line-number-and-way-right":
+//          break
+//        case "editor:move-to-line-number-and-way-left":
+//          break
+//        case "editor:insert-under-line-number":
+//          break
+//        case "editor:select-line-number":
+//          break
+//        case "editor:expand-selection-to-scope":
+//          break
+//        case "editor:click-expand-selection-to-scope":
+//          break
+//        case "editor:select-line-number-range":
+//          break
+//        case "editor:extend-selection-to-line-number":
+//          break
+//        case "editor:insert-from-line-number":
+//          break
+//        case "editor:toggle-comments":
+//          break
+//        case "editor:insert-code-template":
+//          break
+//        case "editor:complete-code-template":
+//          break
+//        case "selection:previous-occurrence":
+//          break
+//        case "selection:next-occurrence":
+//          break
+//        case "selection:extend-to-next-occurrence":
+//          break
+//        case "selection:extend-to-previous-occurrence":
+//          break
+//        case "selection:previous-selection-occurrence":
+//          break
+//        case "selection:next-selection-occurrence":
+//          break
+//        case "selection:range-upward":
+//          break
+//        case "selection:range-downward":
+//          break
+//        case "selection:range-on-current-line":
+//          break
+//        case "selection:previous-word-by-surrounding-characters":
+//          break
+//        case "selection:next-word-by-surrounding-characters":
+//          break
+//        default:
+//          NSLog("not handled")
+//      }
       
       //TODO: do something with the invocation buffer
       connection.invalidate()
--- XPCService/VoiceCodeXPCService.swift
+++ XPCService/VoiceCodeXPCService.swift
@@ -2,10 +2,53 @@
 import Foundation
 import Starscream
 
-@objc class VoiceCodeXPCService: NSObject, VoiceCodeXPCServiceProtocol {
+@objc class VoiceCodeXPCService: NSObject, VoiceCodeXPCServiceProtocol, WebSocketDelegate  {
+
+  var socket: WebSocket
+  var messageReceived: String
   
-  func get_latest_command(withReply: (String) -> ()) {
-    withReply("move-to-line-number")
+  override init() {
+    socket = WebSocket(url: URL(string: "ws://localhost:8081/")!)
+    messageReceived = ""
+    super.init()
+    socket.delegate = self
+    socket.connect()
   }
   
+  func get_latest_command(withReply: (String) -> ()) {
+
+    while(!socket.isConnected) {
+      // NSLog("connecting")
+    }
+    
+    while(messageReceived == "") {
+      // NSLog("waiting for message")
+    }
+    
+    withReply(messageReceived)
+    
+    messageReceived = ""
+  }
+  
+  func websocketDidConnect(socket: WebSocket) {
+    NSLog("websocket is connected")
+    socket.write(string: "getCommand")
+  }
+  
+  func websocketDidDisconnect(socket: WebSocket, error: NSError?) {
+    if let e = error {
+      NSLog("websocket is disconnected: \(e.localizedDescription)")
+    } else {
+      NSLog("websocket disconnected")
+    }
+  }
+  
+  func websocketDidReceiveMessage(socket: WebSocket, text: String) {
+    NSLog("Received text: \(text)")
+    messageReceived = text
+  }
+  
+  func websocketDidReceiveData(socket: WebSocket, data: Data) {
+    NSLog("Received data: \(data.count)")
+  }
 }
