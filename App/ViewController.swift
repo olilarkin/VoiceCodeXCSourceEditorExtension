ace6fdef4d5605ff0be2f860ac5a2509828dccc7
Oli Larkin <olilarkin@googlemail.com>
2017-07-28 19:37:09 +0100

    replace xib with storyboard and add text

---
+++ App/ViewController.swift
@@ -0,0 +1,17 @@
+//  ViewController.swift
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    }
+  }
+
+
+}
+
